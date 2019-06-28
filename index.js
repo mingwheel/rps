4fad601d1c0aead1379ffea16c797139516ca898
Ming <dev.sohml@gmail.com>
2019-06-28 17:58:17 +0800

    Refactor js code and Improve GUI of game

--- index.js
+++ index.js
@@ -1,48 +1,104 @@
-function init() {
-  let rounds = 5;
-  let playerScore = 0;
-  let computerScore = 0;
-  const choices = document.querySelectorAll("#player i");
+const game = {
+  rounds: 5,
+  scores: [0, 0], // [computer, player]
+  options: {
+    rock: 0,
+    paper: 1,
+    scissors: 2
+  },
+  isRunning: false,
+  hasEnded: false,
 
-  choices.forEach(c => {
-    c.addEventListener("click", () => {
-      const playerSelection = c.classList[0];
-      const computerSelection = computerPlay();
-      console.log(playerSelection, computerSelection);
+  init() {
+    const playerChoices = document.querySelectorAll("#player i");
+    const computerChoices = document.querySelectorAll("#computer i");
+    const scores = document.querySelectorAll(".score");
 
-      game(playerSelection, computerSelection);
-      console.log(`Scores: ${playerScore}, ${computerScore}`);
+    playerChoices.forEach(choice => {
+      choice.onmouseover = () => { if (!game.isRunning) choice.classList.add("fas") };
+      choice.onmouseout = () => { if (!game.isRunning) choice.classList.remove("fas") };
+
+
+      choice.addEventListener("click", function () {
+        if (game.isRunning) return;
+
+        game.isRunning = true;
+        const playerSelection = game.options[this.classList[0]];
+        const computerSelection = game.computerPlay();
+
+        const result = game.match(playerSelection, computerSelection);
+        if (result) game.updateScores(result, scores);
+        game.updateDisplay(this, computerChoices[computerSelection], result);
+
+      });
     });
-  });
+  },
 
-  function computerPlay() {
-    plays = ['rock', 'paper', 'scissors'];
-    return plays[Math.floor(Math.random() * 3)];
-  }
+  match(player, computer) {
+    if (game.hasEnded) return;
 
-  function playRound(a, b) {
-    const p = {
-      rock: 0,
-      paper: 1,
-      scissors: 2
-    }
-    return (p[a] === p[b]) ? 0 // Draw
-                            : ((p[a] - p[b] + 3) % 3 === 1) ? 1 // Win
-                            : -1; // Lose
-  }
-
-  function game(player, computer) {
-    if (playerScore === rounds || computerScore === rounds) return;
     const result = playRound(player, computer);
-    if (result === 0) {
-      console.log('draw');
-    } else if (result > 0) {
-      console.log('win');
-      playerScore++;
-    } else { 
-      console.log('lose'); 
-      computerScore++; 
-    }
-  }
-}
+    if (!result) return;
+    else if (result > 0) game.scores[1]++;
+    else game.scores[0]++;
 
+    if (game.scores.includes(game.rounds)) {
+      game.hasEnded = true;
+    }
+
+    return result;
+
+    function playRound(a, b) {
+      // Return 0 = draw, 1 = win, -1 = lose
+      return (a === b) ? 0
+        : ((a - b + 3) % 3) === 1 ? 1
+          : -1;
+    }
+  },
+
+  reset() {
+    game.playerScore = game.computerScore = 0;
+    game.init();
+  },
+
+  updateScores(winner, scores) {
+    if (winner < 0) winner = 0;
+
+    scores[winner].style.opacity = 0;
+    setTimeout(() => {
+      scores[winner].textContent = game.scores[winner];
+      scores[winner].style.opacity = 1;
+    }, 300);
+  },
+
+  updateDisplay(player, computer, result) {
+    const resultDisplay = document.querySelector("h1");
+    if (game.hasEnded) {
+      outcome = (game.scores.indexOf(game.rounds) > 0) ? 'win' : 'lose'
+      resultDisplay.textContent = `You ${outcome}!`
+    }
+
+    if (!result) {
+      player.classList.add('draw');
+      computer.classList.add('draw');
+    } else if (result > 0) {
+      player.classList.add('win');
+      computer.classList.add('lose');
+    } else {
+      player.classList.add('lose');
+      computer.classList.add('win');
+    }
+    player.classList.add('fas');
+    computer.classList.add('fas');
+
+    window.setTimeout(() => {
+      player.classList.remove('fas', 'draw', 'win', 'lose');
+      computer.classList.remove('fas', 'draw', 'win', 'lose');
+      game.isRunning = false;
+    }, 1000);
+  },
+
+  computerPlay() {
+    return Math.floor(Math.random() * 3);
+  },
+}
